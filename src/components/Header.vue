437864900650231684efbe9c512f82da3b888fec
coolburly <dg.victure@gmail.com>
2026-09-03 17:33:51 +0800

    移除 avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -72,10 +72,6 @@ const sitename = computed(() => appStore.publicSettings?.sitename || 'Komari Mon
   >
     <div class="px-4 flex-between h-14 max-w-[1280px] mx-auto">
       <div class="flex items-center gap-3 cursor-pointer" @click="router.push('/')">
-        <Avatar class="size-8">
-          <AvatarImage :src="siteFavicon" :alt="sitename" />
-          <AvatarFallback>{{ sitename.slice(0, 1) }}</AvatarFallback>
-        </Avatar>
         <h3 class="m-0 text-lg font-semibold">
           {{ sitename }}
         </h3>
